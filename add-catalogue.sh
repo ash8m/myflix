4f1798446c31193e9f4b760f069c433f2b8b52da
ash8m <ashishtom674@gmail.com>
2024-01-16 19:30:13 +0000

    changes to script for webserver 2

--- add-catalogue.sh
+++ add-catalogue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -i --header "Content-Type: application/json" --header "Authorization: Basic YWRtaW46c2VjcmV0" --request PUT http://localhost:8080/videos 
-curl -i --header "Content-Type: application/json" --header "Authorization: Basic YWRtaW46c2VjcmV0" --request POST --data "@./services/mongocatalogue/videos.json" http://localhost:8080/videos
+curl -i --header "Content-Type: application/json" --header "Authorization: Basic YWRtaW46c2VjcmV0" --request PUT http://44.198.29.234:8080/videos 
+curl -i --header "Content-Type: application/json" --header "Authorization: Basic YWRtaW46c2VjcmV0" --request POST --data "@./services/mongocatalogue/videos.json" http://44.198.29.234:8080/videos
