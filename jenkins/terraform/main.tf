7e441d627d6ece0174dd734a521036641c8a3d9d
ash8m <ashishtom674@gmail.com>
2024-01-17 03:28:47 +0000

    sepcify terraform credential location

--- jenkins/terraform/main.tf
+++ jenkins/terraform/main.tf
@@ -2,6 +2,7 @@
 # First, add the provider code to ensure you use the AWS provider.
 provider "aws" {
   region     = "us-east-1"
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
 }
 
 data "aws_security_group" "existing_sg" {
